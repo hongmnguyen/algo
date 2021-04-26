7bff696e4093752ed6559443887ef8b5704d552a
Hong Nguyen <hongsap@gmail.com>
2021-04-25 23:12:18 -0700

    fix bugs for BinarySearch2 in case target value not in array

--- src/binarySearch/binarySearch2.java
+++ src/binarySearch/binarySearch2.java
@@ -1,14 +1,16 @@
 package binarySearch;
 
-//1. Tìm index của số đầu tiên nhỏ hơn số cần tìm
-//2. Tìm index của số đầu tiên lớn hơn số cần tìm
+//1. Tìm index của số đầu tiên nhỏ hơn số cần tìm. neu so can tim ko nam trong array, van tra ve so dau tien nho hon
+//2. Tìm index của số đầu tiên lớn hơn số cần tìm. neu so can tim ko nam trong array, van tra ve so dau tien lon hon
 public class binarySearch2{
     public static int binarySearchFirstSmaller(int[] a, int left, int right, int x) { //x is target number
+        int mid=0;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            mid = left + (right - left) / 2;
 
             if (x == a[mid]) {
-                if (mid==0) return -1;
+                if (mid==0) return mid;
+
                 if (a[mid - 1] == a[mid]) {
                     right = mid - 1;
                 } else return mid-1;
@@ -22,14 +24,16 @@ public class binarySearch2{
             }
         }
 
-        return -1;
+        return (mid==a.length-1?mid-1:mid);
     }
     public static int binarySearchFirstBigger(int[] a, int left, int right, int x) { //x is target number
+        int mid=0;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            mid = left + (right - left) / 2;
 
             if (x == a[mid]) {
-                if (mid==a.length-1) return -1;
+                if (mid==a.length-1) return mid;
+
                 if (a[mid+1]==a[mid]) {
                     left = mid + 1;
                 }
@@ -42,15 +46,15 @@ public class binarySearch2{
                 left = mid + 1;
             }
         }
-        return -1;
+        return (mid==0? mid+1: mid);
     }
 
     public static void main(String[] args) {
-        int[] a= {1,2,3,4,4,4,5,6};
+        int[] a= {1,3,3,4,4,4,6,8};
         binarySearch2 b = new binarySearch2();
-        int resFirstSmaller=b.binarySearchFirstSmaller(a,0, a.length-1, 6);
+        int resFirstSmaller=b.binarySearchFirstSmaller(a,0, a.length-1, 4);
         System.out.println(resFirstSmaller);
-        int resFirstBigger=b.binarySearchFirstBigger(a,0, a.length-1, 6);
+        int resFirstBigger=b.binarySearchFirstBigger(a,0, a.length-1, 4);
         System.out.println(resFirstBigger);
 
 
